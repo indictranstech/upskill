6691ff85953d20b13193b32d8df7e56c174dc3a0
Gangadhar <gangadhar.k@indictranstech.com>
2016-07-11 15:42:20 +0530

    added masters

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+version = '0.0.1'
+requirements = parse_requirements("requirements.txt", session="")
+
+setup(
+	name='upskill',
+	version=version,
+	description='This app is for skill tranining',
+	author='New Indictrans Technologies pvt. ltd.',
+	author_email='support@indictranstech.com',
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=[str(ir.req) for ir in requirements],
+	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+)
---
+++ upskill/hooks.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+app_name = "upskill"
+app_title = "Up Skill"
+app_publisher = "New Indictrans Technologies pvt. ltd."
+app_description = "This app is for skill tranining"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_email = "support@indictranstech.com"
+app_version = "0.0.1"
+app_license = "MIT"
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/upskill/css/upskill.css"
+# app_include_js = "/assets/upskill/js/upskill.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/upskill/css/upskill.css"
+# web_include_js = "/assets/upskill/js/upskill.js"
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+#	"Role": "home_page"
+# }
+
+# Website user home page (by function)
+# get_website_user_home_page = "upskill.utils.get_home_page"
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# Installation
+# ------------
+
+# before_install = "upskill.install.before_install"
+# after_install = "upskill.install.after_install"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "upskill.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+#	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"upskill.tasks.all"
+# 	],
+# 	"daily": [
+# 		"upskill.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"upskill.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"upskill.tasks.weekly"
+# 	]
+# 	"monthly": [
+# 		"upskill.tasks.monthly"
+# 	]
+# }
+
+# Testing
+# -------
+
+# before_tests = "upskill.install.before_tests"
+
+# Overriding Whitelisted Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "upskill.event.get_events"
+# }
+
